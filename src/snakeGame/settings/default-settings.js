c8bfb6b4a6272965ae203a4defb8f59e1080cc5f
Borys Ihnatyev <paur.link@gmail.com>
2017-08-21 11:56:04 +0300

    calculate dynamic field size

--- src/snakeGame/settings/default-settings.js
+++ src/snakeGame/settings/default-settings.js
@@ -1,10 +1,16 @@
+const scale = 20
+
 export const defaultSettings = {
-    scale: 20,
+    scale,
     level: 1,
     snakeLength: 10,
     fieldSize: {
-        width: 40,
-        height: 20,
+        width: calcMaxSize(window.screen.availWidth, scale),
+        height: calcMaxSize(window.screen.availHeight, scale),
     },
     transitionDuration: 50
+}
+
+function calcMaxSize(size, scale) {
+    return Math.trunc(size / scale)
 }
